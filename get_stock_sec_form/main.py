c9c5ea758b4b38cf0752d73c1597e0c1adeb51c5
Tom Selfin <selfint@gmail.com>
2021-07-10 17:42:44 +0300

    implement get_stock_sec_form

---
+++ get_stock_sec_form/main.py
@@ -0,0 +1,99 @@
+import requests
+from flask import Request, Response
+from requests.adapters import HTTPAdapter
+from sec_edgar_downloader._constants import (
+    DATE_FORMAT_TOKENS,
+    DEFAULT_AFTER_DATE,
+    DEFAULT_BEFORE_DATE,
+    MAX_RETRIES,
+    SEC_EDGAR_RATE_LIMIT_SLEEP_INTERVAL,
+    SEC_EDGAR_SEARCH_API_ENDPOINT,
+)
+from sec_edgar_downloader._utils import (
+    EdgarSearchApiError,
+    FilingMetadata,
+    build_filing_metadata_from_hit,
+    form_request_payload,
+)
+from urllib3.util.retry import Retry
+
+HEADERS = {"User-Agent": "Vale selfint@gmail.com", "Accept-Encoding": "gzip, deflate"}
+
+
+def get_stock_sec_form(request: Request) -> Response:
+    """
+    HTTP Cloud Function. Fetch SEC form of a stock.
+
+    Returns the HTML of the requested form as the response.
+    """
+    stock = request.json["stock"]
+    form = request.json["form"]
+
+    form_content = _get_stock_sec_form_html(stock.upper(), form.upper())
+    return Response(form_content)
+
+
+def _get_stock_sec_form_html(stock: str, form: str) -> str:
+    """Download latest form for stock and return the form's HTML content"""
+
+    url_metadata = _get_filing_url(form.upper(), stock.upper())
+    url = url_metadata.full_submission_url
+    content = requests.get(url, headers=HEADERS).text
+
+    return content
+
+
+def _get_filing_url(
+    filing_type: str,
+    ticker_or_cik: str,
+) -> FilingMetadata:
+    retries = Retry(
+        total=MAX_RETRIES,
+        backoff_factor=SEC_EDGAR_RATE_LIMIT_SLEEP_INTERVAL,
+        status_forcelist=[403, 500, 502, 503, 504],
+    )
+    client = requests.Session()
+    client.mount("http://", HTTPAdapter(max_retries=retries))
+    client.mount("https://", HTTPAdapter(max_retries=retries))
+    try:
+        payload = form_request_payload(
+            ticker_or_cik,
+            [filing_type],
+            DEFAULT_AFTER_DATE.strftime(DATE_FORMAT_TOKENS),
+            DEFAULT_BEFORE_DATE.strftime(DATE_FORMAT_TOKENS),
+            0,
+            "",
+        )
+        resp = client.post(
+            SEC_EDGAR_SEARCH_API_ENDPOINT,
+            json=payload,
+            headers=HEADERS,
+        )
+        resp.raise_for_status()
+        search_query_results = resp.json()
+
+        if "error" in search_query_results:
+            try:
+                root_cause = search_query_results["error"]["root_cause"]
+                if not root_cause:  # pragma: no cover
+                    raise ValueError
+
+                error_reason = root_cause[0]["reason"]
+                raise EdgarSearchApiError(
+                    f"Edgar Search API encountered an error: {error_reason}. "
+                    f"Request payload: {payload}"
+                )
+            except (ValueError, KeyError):  # pragma: no cover
+                raise EdgarSearchApiError(
+                    "Edgar Search API encountered an unknown error. "
+                    f"Request payload:\n{payload}"
+                )
+
+        hit = search_query_results["hits"]["hits"][0]
+
+        metadata = build_filing_metadata_from_hit(hit)
+
+    finally:
+        client.close()
+
+    return metadata
--- tests/test_get_stock_sec_form.py
+++ tests/test_get_stock_sec_form.py
@@ -1,5 +1,31 @@
-from get_stock_sec_form import __version__
+from get_stock_sec_form.main import _get_stock_sec_form_html
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_get_aapl_10k():
+    expected_first_10_lines = """<SEC-DOCUMENT>0000320193-20-000096.txt : 20201030
+<SEC-HEADER>0000320193-20-000096.hdr.sgml : 20201030
+<ACCEPTANCE-DATETIME>20201029180625
+ACCESSION NUMBER:		0000320193-20-000096
+CONFORMED SUBMISSION TYPE:	10-K
+PUBLIC DOCUMENT COUNT:		99
+CONFORMED PERIOD OF REPORT:	20200926
+FILED AS OF DATE:		20201030
+DATE AS OF CHANGE:		20201029
+"""
+    expected_last_10_lines = """M 0 !E@( %               @ %#P00 86%P;"TR,#(P,#DR-E]G,BYJ<&=0
+M2P$"% ,4    " #)D%U1ZE:6M<)8 0#.O0T %0              @ %D@08
+M86%P;"TR,#(P,#DR-E]L86(N>&UL4$L! A0#%     @ R9!=443SH2\QOP
+MY,,( !4              ( !6=H' &%A<&PM,C R,# Y,C9?<')E+GAM;%!+
+4!08     $  0 $\$  "]F0@    !
+
+end
+</TEXT>
+</DOCUMENT>
+</SEC-DOCUMENT>"""
+
+    aapl_10k_html = _get_stock_sec_form_html("aapl", "10-k")
+    fetched_first_10_lines = "\n".join(aapl_10k_html.splitlines()[:10])
+    fetched_last_10_lines = "\n".join(aapl_10k_html.splitlines()[-10:])
+
+    assert fetched_first_10_lines == expected_first_10_lines
+    assert fetched_last_10_lines == expected_last_10_lines
